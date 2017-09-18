178ca0c8b24f1ae493c62b47651270145457f870
mlarnick <mlarnick@optonline.net>
2017-09-18 15:17:16 -0400

    Added working score counter

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
 
-	// Update is called once per frame
-	void Update () {
+    public Text scoreGT;
+
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+        // Get the Text component of the GameObject
+        scoreGT = scoreGO.GetComponent<Text>();
+
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+    // Update is called once per frame
+    void Update () {
         // Get the current screen position of the mouse from Input
         Vector3 mousePos2d = Input.mousePosition;
 
@@ -29,5 +43,14 @@ public class Basket : MonoBehaviour {
         {
             Destroy(collidedWith);
         }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+
+        // Add points for catching the apple
+        score += 100;
+
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
     }
 }
